4307b4b95b3dbac267b0fced83a088dc34429cb4
JL-Asus_Graznov <graznov@me.com>
2024-11-11 19:15:05 +0300

    1

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Movie = require("./models/movie");
+const movieRoutes = require('./routes/movie-routes');
+
+const PORT = 3000
+const URL =  'mongodb://localhost:27017/moviesbox';
+
+
+const app = express();
+app.use(express.json());
+app.use(movieRoutes)
+
+mongoose
+    .connect(URL)
+    .then(()=>console.log('Connected to MongoDB...'))
+    .catch((err)=>console.log(`DB connection failed: ${err}`));
+
+app.listen(PORT, (err) =>{
+    err ? console.log(err) : console.log(`Listening port ${PORT}`)
+})
